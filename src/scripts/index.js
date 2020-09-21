9adeb1228d4094093e2f62118beed3c887eefae5
Alice-Rez <reznickova.alice@seznam.cz>
2020-09-21 16:28:11 +0200

    first fetch done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,31 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
-
 
 // \/ All of your javascript should go here \/
 
+let input = document.getElementById("city");
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click", getWeather);
+
+function getWeather(event) {
+  event.preventDefault();
+  let city = input.value.toLowerCase();
+
+  console.log(`https://www.metaweather.com/api/location/search/?query=${city}`);
+
+  fetch(
+    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${city}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((jsonData) => {
+      let code = jsonData[0].woeid;
+      console.log(code);
+    })
+    .catch((reject) => {
+      console.log(reject);
+    });
+}
