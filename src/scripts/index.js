d4c137919e2510def17191f01de8e4f291394e50
Alice-Rez <reznickova.alice@seznam.cz>
2020-09-27 22:08:26 +0200

    basic function and style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,110 @@
-// The following line makes sure your styles are included in the project. Don't remove this.
+// ----- IMPORTING -----
+
 import "../styles/main.scss";
-// Import any additional modules you want to include below \/
+import "babel-polyfill"; // to be able to use async await
+import $ from "jquery"; // importing jquery
+const Handlebars = require("handlebars"); // importing handlebars
 
-// \/ All of your javascript should go here \/
+$(document).ready(() => {
+  $(".form").submit(getWeather);
+});
 
-let input = document.getElementById("city");
-let submit = document.getElementById("submit");
-
-submit.addEventListener("click", getWeather);
-
-function getWeather(event) {
+async function getWeather(event) {
   event.preventDefault();
-  let city = input.value.toLowerCase();
 
-  fetch(
-    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${city}`
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((jsonData) => {
-      let code = jsonData[0].woeid;
-      console.log(code);
-    })
-    .catch((reject) => {
-      console.log(reject);
+  $("#results").html("");
+
+  // ----- LOADER ACTIVATING -----
+
+  $(".loader").css("display", "block");
+
+  // ----- FETCHING LOCATION -----
+
+  let responseLocation = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${$(
+      "#city"
+    )
+      .val()
+      .toLowerCase()}`
+  );
+  let locationData = await responseLocation.json();
+
+  if (locationData.length === 0) {
+    $("#results").text("Invalid Input! Location not found");
+    $(".loader").css("display", "none");
+  } else {
+    // ---- FETCHING WEATHER DATA -----
+
+    let code = locationData[0].woeid;
+    let responseWeather = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${code}`
+    );
+    let weatherJson = await responseWeather.json();
+    let weatherData = weatherJson.consolidated_weather;
+
+    // ----- MANIPULATING WEATHER DATA FOR NICER DISPLAY -----
+
+    weatherData.map((element, index) => {
+      if (index === 0) {
+        element.day = "today";
+      } else if (index === 1) {
+        element.day = "tomorrow";
+      } else {
+        element.day = chooseDay(element.applicable_date.substr(0, 10));
+        console.log(element.day);
+      }
+
+      element.max_temp = Math.round(element.max_temp);
+      element.min_temp = Math.round(element.min_temp);
+      element.applicable_date = element.applicable_date
+        .split("-")
+        .reverse()
+        .join(".");
+      element.img_source = `https://www.metaweather.com/static/img/weather/${element.weather_state_abbr}.svg`;
+
+      if (element.wind_speed > 5) {
+        element.wind_img =
+          "https://grid.gograph.com/wind-measurement-instrument-icon-eps-illustration_gg78945909.jpg";
+        element.wind_speed = Math.round(element.wind_speed * 1.60934);
+      }
     });
+
+    // ----- INJECTING DATA TO HTML USING HANDLEBARS -----
+
+    let cardTemp = $("#card-template").html();
+    let handleFunction = Handlebars.compile(cardTemp);
+    let result = handleFunction({ weatherData });
+    $("#results").append(result);
+
+    // ----- DEACTIVATE LOADER -----
+
+    $(".loader").css("display", "none");
+
+    // ----- DISPLAYING WEATHER INFO -----
+
+    $(".weather").css("display", "flex");
+
+    console.log(weatherData);
+    styleWeather(weatherData);
+  }
+}
+
+function styleWeather() {}
+
+function chooseDay(string) {
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = new Date(string);
+
+  console.log(day.getDay());
+
+  return weekdays[day.getDay()];
 }
